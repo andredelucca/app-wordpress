3734df564e2add758d3a50f7e859be11b0271b51
Vendas 02 <andre.delucca80@gmail.com>
2021-03-22 17:06:06 -0300

    Projeto iniciado e finalizado o front-end

---
+++ wp-content/themes/martinluz/index-original.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>Martin Luz</title>
+    <meta name="description" content="<?php bloginfo("description") ?>">
+    <meta name="keywords" content="testando wordpress">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="pingback" href="http://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="wp-content/themes/martinluz/style.css"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">    
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div class="col-first">
+                <div class="content-first">
+                    <p id="text-um">Reembolsos corporativos de forma simples e rápida. <b>Como deve ser</b>.</p>
+                    <p id="text-dois">Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, 
+                    compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando 
+                    aprovações, visualizando relatórios e muito mais!</p>
+                    <p><a class="btn btn-secondary-1" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
+                    <p id="text-tres">Responderemos em até 24h.</p>
+                </div>
+            </div>    
+        </div>
+        <div class="col-6">
+            <div class="col-second">
+                <img id="image-1" style="" src="wp-content/themes/martinluz/images/large-Header_3d_3.1.png" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-6" id="image-2">       
+            <div>
+                <img src="wp-content/themes/martinluz/images/captura-de-tela 18.55.1.png" alt="">
+            </div>
+        </div>
+        <div class="col-6">       
+            <div>
+                <p id="text-quatro">Assuma agora o <b>controle dos gastos</b> variáveis da sua empresa</p>
+                <p id="text-cinco">O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta 
+                digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo 
+                sem custos adicionais para tornar seu controle financeiro mais simples e prático!</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <canvas id="UgCanvas-1"></canvas>
+    <div class="row">
+        <div class="col-6">       
+            <p id="text-seis"> <b>Organizar reembolsos</b> da sua empresa nunca foi tão rápido</p>
+            <p id="text-sete">Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar 
+            transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber 
+            comprovantes e visualizar relatórios que organizam seu reembolso.</p>
+            <p><a class="btn btn-secondary-2" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
+        </div>
+        <div class="col-6" id="image-3">       
+            <div>
+                <img src="wp-content/themes/martinluz/images/bpp_corp_reembolso 1.png" alt="">   
+            </div>
+        </div>
+    </div>
+    <canvas id="UgCanvas-2"></canvas>
+    <div>
+        <p id="text-oito">A plataforma que ajuda a <b>reduzir os gastos</b> do dia a dia</p>
+    </div>
+    <div>
+        <p id="text-nove">Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, 
+        reduzindo custos e aumentando os resultados da sua empresa.</p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row-1">
+        <div class="col-xl-4">
+            <img id="image-4" src="wp-content/themes/martinluz/images/foguete.png" alt="">
+            <p id="text-dez">Mais rapidez na gestão das suas despesas</p>
+            <p id="text-onze">Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados 
+            de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de 
+            forma rápida e digital.</p>
+        </div>
+        <div class="col-xl-4">
+            <img id="image-5" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_RELATORIO 2.png" alt="">
+            <p id="text-doze">Os relatórios que você precisa em um único lugar</p>
+            <p id="text-treze">Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. 
+            Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo 
+            com formulários e planilhas.</p>
+        </div>
+        <div class="col-xl-4">
+            <img id="image-6" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_ORGANIZE 1.png" alt="">
+            <p id="text-quatorze">Organize todos os seus comprovantes</p>
+            <p id="text-quinze">Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais 
+            guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por 
+            projeto, da forma que você precisar.</p>        
+        </div>
+        <div class="w-100"></div>
+        <div class="col-xl-4">
+            <img id="image-7" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_FOTO COMPROVANTE 1.png" alt="">
+            <p id="text-dezeseis">Apresente suas despesas com um só clique</p>
+            <p id="text-dezesete">Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do 
+            cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou 
+            reprovar aquele gasto através do App.</p>
+        </div>
+        <div class="col-xl-4">
+            <img id="image-8" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CARTAO PRE PAGO 1.png" alt="">
+            <p id="text-dezoito">Controle seus gastos com cartões pré-pagos VISA</p>
+            <p id="text-dezenove">Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza
+            para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de 
+            estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+        </div>
+        <div class="col-xl-4">
+            <img id="image-9" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CONTA DIGITAL 1.png" alt="">
+            <p id="text-vinte">Conta digital gratuita e exclusiva para sua empresa</p>
+            <p id="text-vinte-um">Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua 
+            conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+        </div>
+    </div>
+</div>
+
+        
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- wp-content/themes/martinluz/index.php
+++ wp-content/themes/martinluz/index.php
@@ -17,37 +17,38 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div class="container">
-    <div class="row">
-        <div class="col-6">
-            <div class="col-first">
-                <div class="content-first">
-                    <p id="text-um">Reembolsos corporativos de forma simples e rápida. <b>Como deve ser</b>.</p>
-                    <p id="text-dois">Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, 
-                    compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando 
-                    aprovações, visualizando relatórios e muito mais!</p>
-                    <p><a class="btn btn-secondary-1" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
-                    <p id="text-tres">Responderemos em até 24h.</p>
-                </div>
-            </div>    
-        </div>
-        <div class="col-6">
-            <div class="col-first">
-                <img id="image-1" style="" src="wp-content/themes/martinluz/images/large-Header_3d_3.1.png" alt="">
-            </div>
-        </div>
-    </div>
-</div>
+<!--
+
 
 <div class="container">
+    <div class="row-1">
+        
+    </div>
+</div>
+-->
+<div class="container-fluid">
     <div class="row">
-        <div class="col-6" id="image-2">       
-            <div>
-                <img src="wp-content/themes/martinluz/images/captura-de-tela 18.55.1.png" alt="">
+        <div class="col-sm-6">
+            <div class="text-blockone">
+                <p id="text-um">Reembolsos corporativos de forma simples e rápida. <b>Como deve ser</b>.</p>
+                <p id="text-dois">Acabe com a dor de cabeça da gestão de despesas do dia a dia da sua empresa: pagamentos, reembolsos, 
+                compras on-line e gestão do fundo fixo. Controle seus gastos escaneando notas fiscais, acompanhando 
+                aprovações, visualizando relatórios e muito mais!</p>
+                <p><a class="btn btn-secondary-1" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
+                <p id="text-tres">Responderemos em até 24h.</p>
             </div>
         </div>
+        <div class="col-sm-6">
+            <img id="image-1" style="" src="wp-content/themes/martinluz/images/large-Header_3d_3.1.png" alt=""> 
+        </div>
+    </div>
+    
+    <div class="row">
         <div class="col-6">       
-            <div>
+                <img id="image-2" src="wp-content/themes/martinluz/images/Captura de Tela 2019-12-19 às 18.55 1.png" alt="">
+            </div>
+        <div class="col-6">       
+            <div class="text-blocktwo">
                 <p id="text-quatro">Assuma agora o <b>controle dos gastos</b> variáveis da sua empresa</p>
                 <p id="text-cinco">O BPP Corp é uma solução única para a administração de gastos corporativos que conta com uma conta 
                 digital empresarial grátis, cartões VISA pré-pagos para controle total das despesas e um aplicativo 
@@ -55,81 +56,147 @@
             </div>
         </div>
     </div>
-</div>
-
-<div class="container">
+    
     <canvas id="UgCanvas-1"></canvas>
-    <div class="row">
-        <div class="col-6">       
-            <p id="text-seis"> <b>Organizar reembolsos</b> da sua empresa nunca foi tão rápido</p>
-            <p id="text-sete">Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar 
-            transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber 
-            comprovantes e visualizar relatórios que organizam seu reembolso.</p>
-            <p><a class="btn btn-secondary-2" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
-        </div>
-        <div class="col-6" id="image-3">       
-            <div>
-                <img src="wp-content/themes/martinluz/images/bpp_corp_reembolso 1.png" alt="">   
+        <div class="row">
+            <div class="col-6">
+                <div class="text-blockthree">       
+                    <p id="text-seis"> <b>Organizar reembolsos</b> da sua empresa nunca foi tão rápido</p>
+                    <p id="text-sete">Utilizando BPP Corp, você tem controle total sobre o budget de sua equipe podendo realizar 
+                    transferências instantâneas para os cartões corporativos dos colaboradores, acompanhar gastos, receber 
+                    comprovantes e visualizar relatórios que organizam seu reembolso.</p>
+                    <p><a class="btn btn-secondary-2" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
+                </div>
+            </div>
+            <div class="col-6">       
+                <img id="image-3" src="wp-content/themes/martinluz/images/bpp_corp_reembolso 1.png" alt=""> 
             </div>
         </div>
-    </div>
+    
+    <div class="row">
     <canvas id="UgCanvas-2"></canvas>
-    <div>
-        <p id="text-oito">A plataforma que ajuda a <b>reduzir os gastos</b> do dia a dia</p>
-    </div>
-    <div>
-        <p id="text-nove">Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, 
-        reduzindo custos e aumentando os resultados da sua empresa.</p>
-    </div>
-</div>
-
-<div class="container">
-    <div class="row-1">
-        <div class="col-xl-4">
-            <img id="image-4" src="wp-content/themes/martinluz/images/foguete.png" alt="">
-            <p id="text-dez">Mais rapidez na gestão das suas despesas</p>
-            <p id="text-onze">Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados 
-            de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de 
-            forma rápida e digital.</p>
+        <div class="text-blockfour">    
+            <div>
+                <p id="text-oito">A plataforma que ajuda a <b>reduzir os gastos</b> do dia a dia</p>
+            </div>
+            <div>
+                <p id="text-nove">Simplifique a gestão das despesas, evitando a perda de tempo com comprovantes e planilhas, 
+                reduzindo custos e aumentando os resultados da sua empresa.</p>
+            </div>
         </div>
-        <div class="col-xl-4">
-            <img id="image-5" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_RELATORIO 2.png" alt="">
-            <p id="text-doze">Os relatórios que você precisa em um único lugar</p>
-            <p id="text-treze">Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. 
-            Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo 
-            com formulários e planilhas.</p>
-        </div>
-        <div class="col-xl-4">
-            <img id="image-6" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_ORGANIZE 1.png" alt="">
-            <p id="text-quatorze">Organize todos os seus comprovantes</p>
-            <p id="text-quinze">Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais 
-            guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por 
-            projeto, da forma que você precisar.</p>        
+        <div class="grid-one">
+            <div class="col-sm-3">
+                <img id="image-4" src="wp-content/themes/martinluz/images/foguete.png" alt="">
+                <p id="text-dez">Mais rapidez na gestão das suas despesas</p>
+                <p id="text-onze">Para que seu negócio cresça de maneira inteligente e rápida, substitua os processos demorados 
+                de reembolsos, aprovações, criação de relatórios e etc. por uma plataforma que te oferece tudo isso de 
+                forma rápida e digital.</p>
+            </div>
+            <div class="col-sm-3">
+                <img id="image-5" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_RELATORIO 2.png" alt="">
+                <p id="text-doze">Os relatórios que você precisa em um único lugar</p>
+                <p id="text-treze">Encontre relatórios detalhados sobre as utilizações de cada cartão corporativo cadastrado em sua equipe. 
+                Confira o saldo de cada cartão, redistribua valores, faça transferências instantâneas e não perca tempo 
+                com formulários e planilhas.</p>
+            </div>
+            <div class="col-sm-3">
+                <img id="image-6" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_ORGANIZE 1.png" alt="">
+                <p id="text-quatorze">Organize todos os seus comprovantes</p>
+                <p id="text-quinze">Recebendo os comprovantes de gastos de seus colaboradores através da plataforma, você não precisa mais 
+                guardar comprovantes e notas fiscais em lugar nenhum. Os gastos podem ser classificados por tipo ou por 
+                projeto, da forma que você precisar.</p>        
+            </div>
         </div>
         <div class="w-100"></div>
-        <div class="col-xl-4">
-            <img id="image-7" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_FOTO COMPROVANTE 1.png" alt="">
-            <p id="text-dezeseis">Apresente suas despesas com um só clique</p>
-            <p id="text-dezesete">Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do 
-            cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou 
-            reprovar aquele gasto através do App.</p>
+        <div class="grid-two">
+            <div class="col-sm-3">
+                <img id="image-7" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_FOTO COMPROVANTE 1.png" alt="">
+                <p id="text-dezeseis">Apresente suas despesas com um só clique</p>
+                <p id="text-dezesete">Enviar seus comprovantes é mais simples do que você imagina. Basta que o colaborador tire uma foto do 
+                cupom fiscal com seu smartphone através do aplicativo BPP Card. O gestor das despesas pode aprovar ou 
+                reprovar aquele gasto através do App.</p>
+            </div>
+            <div class="col-sm-3">
+                <img id="image-8" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CARTAO PRE PAGO 1.png" alt="">
+                <p id="text-dezoito">Controle seus gastos com cartões pré-pagos VISA</p>
+                <p id="text-dezenove">Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza
+                para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de 
+                estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+            </div>
+            <div class="col-sm-3">
+                <img id="image-9" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CONTA DIGITAL 1.png" alt="">
+                <p id="text-vinte">Conta digital gratuita e exclusiva para sua empresa</p>
+                <p id="text-vinte-um">Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua 
+                conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+            </div>
         </div>
-        <div class="col-xl-4">
-            <img id="image-8" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CARTAO PRE PAGO 1.png" alt="">
-            <p id="text-dezoito">Controle seus gastos com cartões pré-pagos VISA</p>
-            <p id="text-dezenove">Os cartões pré-pago BPP Corp são fáceis de controlar: o colaborador só gasta o que a empresa disponibiliza
-            para compras online ou em lojas físicas. Conte com cartões VISA aceitos em mais de 30 milhões de 
-            estabelecimentos no mundo todo, incluindo sites e aplicativos!</p>
+    
+    <div class="row">
+        <div>
+            <img id="image-10" src="wp-content/themes/martinluz/images/Rectangle 39.png" alt="">
+            <img id="image-11" src="wp-content/themes/martinluz/images/Rectangle 9.png" alt="">
         </div>
-        <div class="col-xl-4">
-            <img id="image-9" src="wp-content/themes/martinluz/images/20200109_BPP_CORP_LP ICONS_CONTA DIGITAL 1.png" alt="">
-            <p id="text-vinte">Conta digital gratuita e exclusiva para sua empresa</p>
-            <p id="text-vinte-um">Oferecemos uma conta digital gratuita especialmente para gerenciar despesas.  Transfira o saldo de sua 
-            conta para os cartões de maneira instantânea, sempre livre de taxas de manutenção ou de juros.</p>
+        <div>
+            <img id="image-13" src="wp-content/themes/martinluz/images/Rectangle 45.png" alt="">
+            <img id="image-14" src="wp-content/themes/martinluz/images/Rectangle 10.png" alt="">
+        </div>
+        <div>
+            <img id="image-15" src="wp-content/themes/martinluz/images/Rectangle 46.png" alt="">
+            <img id="image-16" src="wp-content/themes/martinluz/images/Rectangle 12.png" alt="">
+        </div>
+        <div>
+            <img id="image-17" src="wp-content/themes/martinluz/images/Rectangle 47.png" alt="">
+            <img id="image-18" src="wp-content/themes/martinluz/images/Rectangle 11.png" alt="">
+        </div>
+        <div>
+            <p id="text-vinte-dois">Conheça quem já conta com as <b>vantagens excluisivas</b> do BPP Corp</p>
+            <p id="text-vinte-tres">A empresas mais bem-sucedidas do mundo, grandes e pequenas, confiam no BPP 
+            Corp para reduzir custos com organização dos gastos da suas empresas. </p>
         </div>
     </div>
+    <div class="row">
+        <p id="text-vinte-quatro">Compare e <b>decida agora</b> mesmo</p>
+        <p id="text-vinte-cinco">Substitua o uso do dinheiro em espécie pelo BPP Corp e ganhe segurança e praticidade, 
+        evitando roubos e perdas, tendo uma plataforma única para gerir suas despesas.</p>
+        <img id="image-19" src="wp-content/themes/martinluz/images/Group 179.png" alt="">
+        <p id="text-vinte-seis"><b>Dinheiro em papel</b></p>
+        <p id="text-vinte-sete">Saque de dinheiro em papel para as despesas </p>
+        <p id="text-vinte-oito">Várias notas fiscais em papel</p>
+        <p id="text-vinte-nove">Diversas planilhas de gestão</p>
+        <p id="text-trinta">Problemas de contabilização dos gastos</p>
+        <p id="text-trinta-um">Não tem segurança</p>
+        <img id="image-20" src="wp-content/themes/martinluz/images/Line21.png" alt="">
+        <img id="image-21" src="wp-content/themes/martinluz/images/Group.png" alt="">
+        <p id="text-trinta-dois"><b>BPP Corp</b></p>
+        <p id="text-trinta-tres">Faça pagamentos com o BPP Corp sem precisar tirar dinheiro</p>
+        <p id="text-trinta-quatro">Prestação de contas sem papel, via app</p>
+        <p id="text-trinta-cinco">Relatórios completos em um único lugar</p>
+        <p id="text-trinta-seis">Controle dos gastos com cartões pré-pago</p>
+        <p id="text-trinta-sete">Segurança total do seu dinheiro</p>  
+    </div>
+    <div class="row">
+        <img id="image-22" src="wp-content/themes/martinluz/images/Rectangle 44.png" alt="">    
+        <p id="text-trinta-oito">A sua gestão financeira nunca foi tão fácil.</p>
+        <p id="text-trinta-nove">Conte com as soluções do BPP Corp e facilite o controle dos gastos da sua empresa 
+        agora mesmo.</p>
+        <div class="btn btn-secondary-3">
+            <p><a class="btn-text" href="#" role="button">PEÇA JÁ SUA PROPOSTA</a></p>
+        </div>
+        <p id="text-quarenta">Proposta grátis e sem compromisso.</p>    
+        <p id="text-quarenta-um">Últimos posts</p>
+        <img id="image-23" src="wp-content/themes/martinluz/images/Rectangle 32.png" alt="">
+        <p id="text-quarenta-dois">LOREM IPSUM</p>
+        <img id="image-24" src="wp-content/themes/martinluz/images/Rectangle 33.png" alt="">
+        <p id="text-quarenta-tres">LOREM IPSUM</p>
+        <img id="image-25" src="wp-content/themes/martinluz/images/Rectangle 34.png" alt="">
+        <p id="text-quarenta-quatro">LOREM IPSUM</p>
+        <p id="text-quarenta-cinco">Esta página foi útil?</p>
+        <img id="image-26" src="wp-content/themes/martinluz/images/sad 1.png" alt="">
+        <img id="image-27" src="wp-content/themes/martinluz/images/indifferent 1.png" alt="">
+        <img id="image-28" src="wp-content/themes/martinluz/images/happiness 1.png" alt="">
+        <img id="image-29" src="wp-content/themes/martinluz/images/happiness 2.png" alt="">
+    </div>
 </div>
-
         
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
